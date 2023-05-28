8e46b249536475d3276a7322cec7b63724fe53ce
cookcookcoo <1468023504@qq.com>
2023-05-28 17:03:36 +0800

    完成底部导航栏+各fragment间跳转

---
+++ app/src/main/java/com/example/visualist/HomeFragment.kt
@@ -0,0 +1,24 @@
+package com.example.visualist
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // 在这里可以对布局中的控件进行操作和设置事件监听器
+    }
+}

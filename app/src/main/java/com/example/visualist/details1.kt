749a2db759b1ab4b5bd66588d51d652a773124c0
cookcookcoo <1468023504@qq.com>
2023-05-29 20:21:06 +0800

    完成商品details页面

--- app/src/main/java/com/example/visualist/details1.kt
+++ app/src/main/java/com/example/visualist/details1.kt
@@ -2,10 +2,29 @@ package com.example.visualist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
 
 class details1 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_details1)
+
+        //toolbar返回键设置
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+//        toolbar返回键逻辑实现
+        when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }

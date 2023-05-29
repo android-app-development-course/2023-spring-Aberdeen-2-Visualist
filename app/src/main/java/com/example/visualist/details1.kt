ee4bcfaab30f5079505126f58a453af56ea5f8f4
cookcookcoo <1468023504@qq.com>
2023-05-29 21:38:07 +0800

    完成商品details按钮被点击出现比标识

--- app/src/main/java/com/example/visualist/details1.kt
+++ app/src/main/java/com/example/visualist/details1.kt
@@ -2,8 +2,14 @@ package com.example.visualist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.view.MenuItem
+import android.view.View
+import android.widget.Button
+import android.widget.FrameLayout
+import android.widget.ImageView
 import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
 
 class details1 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,6 +20,35 @@ class details1 : AppCompatActivity() {
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        //按钮被点击后返回标识
+        val addButton = findViewById<Button>(R.id.addCart)
+        val buyButton = findViewById<Button>(R.id.buyDirectly)
+        val successAddImageView = findViewById<ImageView>(R.id.successAddImageView)
+        val successBuyImageView = findViewById<ImageView>(R.id.successBuyImageView)
+        //添加成功
+        addButton.setOnClickListener{
+            //展示
+            successAddImageView.visibility = View.VISIBLE
+            // 延迟一定时间后隐藏标识
+            val handler = Handler()
+            val delayMillis: Long = 1500 // 设置延迟时间，单位为毫秒
+            handler.postDelayed({
+                successAddImageView.visibility = View.INVISIBLE
+            }, delayMillis)
+        }
+        //购买成功
+        buyButton.setOnClickListener{
+            //展示
+            successBuyImageView.visibility = View.VISIBLE
+            // 延迟一定时间后隐藏标识
+            val handler = Handler()
+            val delayMillis: Long = 1500 // 设置延迟时间，单位为毫秒
+            handler.postDelayed({
+                successBuyImageView.visibility = View.INVISIBLE
+            }, delayMillis)
+        }
+
 
     }
 

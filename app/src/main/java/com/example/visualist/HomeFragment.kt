75e6b1a17147e7ff7e702f10c1133a2670fc9ad8
cookcookcoo <1468023504@qq.com>
2023-05-30 20:43:18 +0800

    完成编辑个人资料（Home的二级页面）

--- app/src/main/java/com/example/visualist/HomeFragment.kt
+++ app/src/main/java/com/example/visualist/HomeFragment.kt
@@ -1,9 +1,11 @@
 package com.example.visualist
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 
 class HomeFragment : Fragment() {
@@ -19,6 +21,12 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // 在这里可以对布局中的控件进行操作和设置事件监听器
+        // 点击头像后跳转编辑资料的页面
+        val avatar = view.findViewById<ImageView>(R.id.imageView2)
+        avatar.setOnClickListener{
+            val intent = Intent(getActivity(), editInformation::class.java)
+            startActivity(intent)
+        }
+
     }
 }
---
+++ app/src/main/java/com/example/visualist/details_of_work.kt
@@ -0,0 +1,11 @@
+package com.example.visualist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class details_of_work : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details_of_work)
+    }
+}

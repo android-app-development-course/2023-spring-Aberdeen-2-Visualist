7e5e4c967f81635aa63585c63ce2983c84fed7f6
cookcookcoo <1468023504@qq.com>
2023-06-06 19:54:54 +0800

    修改store页面的布局

---
+++ app/src/main/java/com/example/visualist/identifyPicture.kt
@@ -0,0 +1,11 @@
+package com.example.visualist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class identifyPicture : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_identify_picture)
+    }
+}
